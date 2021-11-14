e72f61ba06011717155d8516978ed82dfcefc598
LinuxHistory <41809645+linuxhistoryblg@users.noreply.github.com>
2021-11-14 15:20:36 -0500

    Update gitea_install.sh

--- gitea_install.sh
+++ gitea_install.sh
@@ -3,7 +3,8 @@ I included the gitea binary in-line with the installation/configuration script s
 Unfortunately, this makes the file larger than the 25MB limit set by GitHub.
 
 gitea_install.sh can be downloaded at:
-https://drive.google.com/file/d/1d11wdmTv_Pbz6rWMxC4YFn7ak0IxRZjr/view?usp=sharing
+https://drive.google.com/file/d/1MLb01Vsgm27hbwrp1c8eYebkwdGqncfs/view?usp=sharing
 
 It is 137MB in size and has the following sha256 hash:
-9a622e81e894f5930a01a4e4980ca66fa93037c3ef2a7ae03d6663f22676ec37  install_gitea.sh
+6fd4e820e56fc460d1c1099f54d54d0bfc069e4caec3a68430a7c65960e81569  install_gitea.sh
+
